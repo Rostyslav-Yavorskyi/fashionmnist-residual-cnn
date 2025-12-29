8d7cfe754251c716dc1658fabe269753adda5ad0
Rostyslav Yavorskyi <yavorskyi.rostik@gmail.com>
2025-12-29 12:28:06 +0200

    feat: add baseline cnn model

--- model.py
+++ model.py
@@ -16,3 +16,29 @@ class MLP(nn.Module):
         return self.net(x)
 
 
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(3136, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+        
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x)
+        return x
+
